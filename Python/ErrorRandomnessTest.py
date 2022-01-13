46eb264f3b42aec452dbc1d757635cddc0927d44
doctor <fredyvides@gmail.com>
2022-01-13 17:52:52 +0000

    Updated PyTJAE repository

--- Python/ErrorRandomnessTest.py
+++ Python/ErrorRandomnessTest.py
@@ -7,10 +7,13 @@ Randomness Test Function for Errors
 """
 
 def ErrorRandomnessTest(idx,X,Xp):
-    from matplotlib.pyplot import hist,subplot,figure
+    from matplotlib.pyplot import hist,subplot,figure,grid,boxplot,semilogy
     from scipy.stats import normaltest
     from numpy import real
     from randtest import random_score
+    from sklearn.metrics import mean_squared_error
+    from math import sqrt
+    
     Noise = real(X[idx[0],:]-Xp[idx[0],:])
     k1,p0 = normaltest(Noise)
     fig_0 = figure(1)
@@ -24,4 +27,12 @@ def ErrorRandomnessTest(idx,X,Xp):
     h0 = hist(Noise,bins=30,density=1,alpha=0.6, color='b')
     print('Randomness test result: ',random_score(Noise))
     print('Normality test p-value: ',p0)
+    rmse = []
+    for k in range(X.shape[0]):
+        rmse.append(sqrt(mean_squared_error(real(X[k,:]), real(Xp[k,:]))))
     fig_0.savefig('fig_results_summary_0'+str(3)+'.png',dpi=600,format='png')
+    fig_1 = figure(2)
+    subplot(211)
+    semilogy(range(1,X.shape[0]+1),rmse),semilogy(range(1,X.shape[0]+1),rmse,'ro'),grid('on')
+    subplot(212),boxplot(rmse),grid('on')
+    fig_1.savefig('fig_results_summary_0'+str(4)+'.png',dpi=600,format='png')
